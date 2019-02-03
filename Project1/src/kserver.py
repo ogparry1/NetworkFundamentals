8279d933ddeb8185105cb327f9422958b0dbf16c
John O. Parry <ogparry1@ufl.edu>
2019-02-03 17:06:52 -0500

    Started adding the sqlite database

--- Project1/src/kserver.py
+++ Project1/src/kserver.py
@@ -1,10 +1,19 @@
 from socket import *
-import sys
-import re
-import json
+import sys, re, json, io
+import sqlite3 as sql
 
 terminate = False
 d = True if '-d' in sys.argv else False
+conn = sql.connect('project1.db')
+db = conn.cursor()
+checkTables = db.execute("SELECT count(*) FROM sqlite_master;")
+print(checkTables)
+if checkTables == '0':
+    print('Creating Tables')
+    db.execute("CREATE TABLE Questions(Number INTEGER PRIMARY KEY ASC, Question TEXT, Correct TEXT);")
+    db.execute("CREATE TABLE Tags(Number INTEGER PRIMARY KEY ASC, tag1 BLOB, tag2 BLOB, tag3 BLOB, tag4 BLOB, tag5 BLOB);")
+    db.execute("CREATE TABLE Answers(Number INTEGER PRIMARY KEY ASC, A TEXT, B TEXT, C TEXT, D TEXT);")
+    conn.commit()
 
 serverPort = 12000
 serverSocket = socket(AF_INET,SOCK_STREAM)
@@ -26,6 +35,35 @@ def buildResponse(arr):
         response = response + e + '\n'
     return response
 
+def addQuestion(newQuestion):
+    qNum = -1
+    tags = re.split(',|, ', request[1])
+    question = request[2]
+    answers = request[3:6]
+    correct = request[7]
+    
+    return str(qNum)
+
+def deleteQuestion(num):
+    question = 'Error: Question ' + num + ' not found.'
+    return question
+
+def getQuestion(num):
+    question = 'Error: Question ' + num + ' not found.'
+    return question
+
+def getRandomQuestion():
+    question = 'Error: No questions exist'
+    return question
+
+def checkAnswer(num, ans):
+    result = 'Error: No questions exist'
+    return result
+
+def helpPage():
+    hPage = 'Help Page'
+    return hPage
+
 ## Start of the Program ##
 print('The server is ready to receive')
 while True:
@@ -41,38 +79,34 @@ while True:
         req = request[0]
 
         # Take arguments and service the request
-        if req == 'k':
+        if req in ['k','kill']:
             sendResponse(connectionSocket,'EXIT')
             connectionSocket.close()
             terminate = True
+            conn.close()
             break
-        elif req == 'q':
+        elif req in ['q','quit']:
             sendResponse(connectionSocket,'EXIT')
             connectionSocket.close()
             break
-        elif req == 'p':
-            tags = re.split(',|, ', request[1])
-            question = request[2]
-            answers = request[3:6]
-            correct = request[7]
+        elif req in ['p','put']:
             response = buildResponse(request)
-
-            # temporary response
             sendResponse(connectionSocket, response)
-        elif req == 'd':
-            # temporary response
-            sendResponse(connectionSocket, req)
-        elif req == 'g':
-            # temporary response
-            sendResponse(connectionSocket, req)
-        elif req == 'c':
-            # temporary response
-            sendResponse(connectionSocket, req)
-        elif req == 'r':
-            # temporary response
-            sendResponse(connectionSocket, req)
-        elif req == 'h':
-            # temporary response
-            sendResponse(connectionSocket, req)
+        elif req in ['d','delete']:
+            response = deleteQuestion(request[1])
+            sendResponse(connectionSocket, response)
+        elif req in ['g','get']:
+            response = getQuestion(request[1])
+            sendResponse(connectionSocket, response)
+        elif req in ['c','check']:
+            response = checkAnswer(request[1], request[2])
+            sendResponse(connectionSocket, response)
+        elif req in ['r','random']:
+            response = getRandomQuestion()
+            sendResponse(connectionSocket, response)
+        elif req in ['h','help']:
+            response = helpPage()
+            sendResponse(connectionSocket, response)
         else:
+            sendResponse(connectionSocket, 'Error: Invalid Request')
             continue
